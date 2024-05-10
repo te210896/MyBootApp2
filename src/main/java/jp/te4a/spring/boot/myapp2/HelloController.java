ca4c7957cf8252daefba6b7d2a1a9ba559738f47
中川 詩月 <210896@jc-21.jp>
2024-05-10 12:25:52 +0900

    made at 2024_05_10

---
+++ src/main/java/jp/te4a/spring/boot/myapp2/HelloController.java
@@ -0,0 +1,12 @@
+package jp.te4a.spring.boot.myapp2;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/taro")
+    public String taro() {
+        return "Taro desu!";
+    }
+}
